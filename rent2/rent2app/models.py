1bdb6cab9c79f6e45ce7254a76f34dc2b245ef8c
itsnitinvishwakarma <71758558+itsnitinvishwakarma@users.noreply.github.com>
2020-12-10 14:54:15 +0530

    removed comments

--- rent2/rent2app/models.py
+++ rent2/rent2app/models.py
@@ -1,26 +1,5 @@
 from django.db import models
 
-# class LoginModel(models.Model):
-#     username=models.CharField(max_length=30,primary_key=True)
-#     password=models.CharField(max_length=20)
-#     email   =   models.EmailField()
-#     contact=models.IntegerField()
-#     class Meta:
-#         abstract=True
-#
-# class CustomerModel(LoginModel):
-#     cus_name  =   models.CharField(max_length=30)
-#     cus_adhar =   models.ImageField(upload_to='adhar/')
-#     cus_addr=models.TextField()
-#     def __str__(self):
-#         return self.username
-#
-# class ItemModel(models.Model):
-#     item_id=models.AutoField(primary_key=True)
-#     item_name=models.CharField(max_length=20)
-#     item_price=models.IntegerField()
-#     item_image=models.ImageField(upload_to='item_images/')
-#     customer=models.ForeignKey(CustomerModel,on_delete=models.CASCADE)
 
 class Login(models.Model):
     username=models.CharField(max_length=30,primary_key=True)
